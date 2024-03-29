07e3e2eb8731168d15f047466f41ca3cd77c37c1
RomanHlodann <roman.hlodann@gmail.com>
2024-03-29 15:45:34 +0200

    added views and urls for crud operations of worker

--- planner/urls.py
+++ planner/urls.py
@@ -4,7 +4,12 @@ from planner.views import (
     TaskTypeListView,
     TaskTypeCreateView,
     TaskTypeUpdateView,
-    TaskTypeDeleteView
+    TaskTypeDeleteView,
+    WorkerListView,
+    WorkerCreateView,
+    WorkerDetailView,
+    WorkerUpdateView,
+    WorkerDeleteView
 )
 
 
@@ -13,6 +18,11 @@ urlpatterns = [
     path('task-type/create', TaskTypeCreateView.as_view(), name="task-type-create"),
     path('task-type/<int:pk>/update', TaskTypeUpdateView.as_view(), name="task-type-update"),
     path('task-type/<int:pk>/delete', TaskTypeDeleteView.as_view(), name="task-type-delete"),
+    path('worker/', WorkerListView.as_view(), name="worker-list"),
+    path('worker/create', WorkerCreateView.as_view(), name="worker-create"),
+    path('worker/<int:pk>/', WorkerDetailView.as_view(), name="worker-detail"),
+    path('worker/<int:pk>/update', WorkerUpdateView.as_view(), name="worker-update"),
+    path('worker/<int:pk>/delete', WorkerDeleteView.as_view(), name="worker-delete"),
 ]
 
 app_name = "planner"
--- planner/views.py
+++ planner/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from planner.models import TaskType
+from planner.forms import WorkerCreationForm
+from planner.models import TaskType, Worker
 
 
 class TaskTypeListView(generic.ListView):
@@ -26,3 +27,28 @@ class TaskTypeDeleteView(generic.DeleteView):
     model = TaskType
     fields = "__all__"
     success_url = reverse_lazy("planner:task-type-list")
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    paginate_by = 10
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    form_class = WorkerCreationForm
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+    queryset = Worker.objects.prefetch_related("tasks")
+
+
+class WorkerUpdateView(generic.UpdateView):
+    model = Worker
+    success_url = reverse_lazy("planner:worker-list")
+
+
+class WorkerDeleteView(generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("planner:worker-list")
