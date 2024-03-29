f82025984ef3bdb2c7b2baf3c24c848c28a53335
RomanHlodann <roman.hlodann@gmail.com>
2024-03-29 13:28:12 +0200

    add views for list, create, update tasktype

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from planner.models import TaskType
+
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    paginate_by = 10
+
+
+class TaskTypeCreateView(generic.CreateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("planner:task-type-list")
+
+
+class TaskTypeUpdateView(generic.UpdateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("planner:task-type-list")
