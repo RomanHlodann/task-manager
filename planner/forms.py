69699feb59acf8a3858c91757093bc0124340bc4
RomanHlodann <roman.hlodann@gmail.com>
2024-04-02 18:08:54 +0300

    add form for creating task

--- planner/forms.py
+++ planner/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from planner.models import Worker
+from planner.models import Worker, Task
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -13,3 +13,18 @@ class WorkerCreationForm(UserCreationForm):
             "last_name",
             "position"
         )
+
+
+class TaskCreationForm(forms.ModelForm):
+    deadline = forms.DateTimeField(
+        widget=forms.SelectDateWidget
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
