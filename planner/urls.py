80b0e2613fe84e57dc5ae25b071a72a65c596520
RomanHlodann <roman.hlodann@gmail.com>
2024-04-05 14:27:20 +0300

    add home url, url to make task completed and / at the end of each url

--- planner/urls.py
+++ planner/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from planner.views import (
+    index,
+    change_to_completed_task,
     TaskTypeListView,
     TaskTypeCreateView,
     TaskTypeUpdateView,
@@ -23,24 +25,26 @@ from planner.views import (
 
 
 urlpatterns = [
-    path('task-type/', TaskTypeListView.as_view(), name="task-type-list"),
-    path('task-type/create', TaskTypeCreateView.as_view(), name="task-type-create"),
-    path('task-type/<int:pk>/update', TaskTypeUpdateView.as_view(), name="task-type-update"),
-    path('task-type/<int:pk>/delete', TaskTypeDeleteView.as_view(), name="task-type-delete"),
-    path('worker/', WorkerListView.as_view(), name="worker-list"),
-    path('worker/create', WorkerCreateView.as_view(), name="worker-create"),
-    path('worker/<int:pk>/', WorkerDetailView.as_view(), name="worker-detail"),
-    path('worker/<int:pk>/update', WorkerUpdateView.as_view(), name="worker-update"),
-    path('worker/<int:pk>/delete', WorkerDeleteView.as_view(), name="worker-delete"),
-    path('task/', TaskListView.as_view(), name="task-list"),
-    path('task/create', TaskCreateView.as_view(), name="task-create"),
-    path('task/<int:pk>/', TaskDetailView.as_view(), name="task-detail"),
-    path('task/<int:pk>/update', TaskUpdateView.as_view(), name="task-update"),
-    path('task/<int:pk>/delete', TaskDeleteView.as_view(), name="task-delete"),
-    path('position/', PositionListView.as_view(), name="task-list"),
-    path('position/create', PositionCreateView.as_view(), name="task-create"),
-    path('position/<int:pk>/update', PositionUpdateView.as_view(), name="task-update"),
-    path('position/<int:pk>/delete', PositionDeleteView.as_view(), name="task-delete"),
+    path("", index, name="index"),
+    path("task-type/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task-type/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
+    path("task-type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
+    path("task-type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
+    path("worker/", WorkerListView.as_view(), name="worker-list"),
+    path("worker/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("worker/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("worker/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("worker/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
+    path("task/", TaskListView.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/change_to_completed", change_to_completed_task, name="task-change_to_completed"),
+    path("position/", PositionListView.as_view(), name="position-list"),
+    path("position/create/", PositionCreateView.as_view(), name="position-create"),
+    path("position/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
+    path("position/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
 ]
 
 app_name = "planner"
